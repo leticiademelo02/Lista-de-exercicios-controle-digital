b0832950ec727b4844ea8fda30db296ab4a418ee
André Carmona Hernandes <andre.hernandes@ufscar.br>
2021-02-12 08:21:54 -0300

    Added aliasing.py and MIT license file

---
+++ intro/aliasing.py
@@ -0,0 +1,19 @@
+################################################################################################################
+#
+#               DIGITAL CONTROL - EE/UFSCAR
+#
+#   Author: André Carmona Hernandes
+#   Version: 1
+#   Last-Update: 12.02.2021
+#
+#   Info: Function to show how Aliasing works
+#
+#   These codes are used in DIGITAL CONTROL classes. You may use and study by them, however use with caution!
+#
+################################################################################################################
+
+from control.matlab import *
+import matplotlib.pyplot as plt
+import numpy
+
+
