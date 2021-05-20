a2094a7028dfc7ef49e6f4d75914e91f88dee897
André Carmona Hernandes <andre.hernandes@ufscar.br>
2021-05-20 15:36:46 -0300

    Modified gain_control and added direct Root Locus

--- state_space/pendulo_1.py
+++ state_space/pendulo_1.py
@@ -105,7 +105,7 @@ for i, _ts in enumerate(Ts):
 
 ### observador
 tt_s = 0.07
-ss_d = c2d(cont_ss, 0.07)
+ss_d = c2d(cont_ss, tt_s)
 p_o1 = np.exp(10*p_c1*tt_s)
 p_o2 = np.exp(10*p_c2*tt_s)
 X0 = [[theta0], [0]]
@@ -113,3 +113,67 @@ Ltr = place(ss_d.A.T, ss_d.C.T, [p_o1, p_o2])
 L = Ltr.T
 print(L)
 # compare
+newA = ss_d.A - np.matmul(L, ss_d.C)
+ol_ss = ss(newA, [[0], [0]], ss_d.C, ss_d.D, tt_s)
+time_o = linspace(0, int(final_time / tt_s) * tt_s, int(final_time / tt_s) + 1)
+yout, tout, xout = initial(ol_ss, time_o, init_state, return_x=True)
+fig, axs = plt.subplots(2, 1)
+axs[0].step(tout, np.rad2deg(xout[:, 0].T), 'r', where='post', label='x1_error')
+axs[1].step(tout, np.rad2deg(xout[:, 1].T), 'b', where='post', label='x2_error')
+axs[0].set(xlabel='time', ylabel='theta [deg]')
+axs[1].set(xlabel='time', ylabel='theta_dot [deg/s]')
+axs[0].set_title('theta')
+axs[1].set_title('theta_dot')
+axs[0].legend(loc='best', shadow=True, fontsize='x-large')
+axs[1].legend(loc='best', shadow=True, fontsize='x-large')
+plt.show()
+
+#putting all together
+#To simulate all together, we shall expande the matrix representation
+p_d1 = np.exp(p_c1 * tt_s)
+p_d2 = np.exp(p_c2 * tt_s)
+K = place(ss_d.A, ss_d.B, [p_d1, p_d2])
+all_A = np.concatenate((ss_d.A.A, -np.matmul(ss_d.B, K)), axis=1)
+aux_A = np.concatenate((np.matmul(L, ss_d.C), newA-np.matmul(ss_d.B, K)), axis=1)
+all_A = np.concatenate((all_A, aux_A), axis=0)
+
+print(all_A)
+aug_ss = ss(all_A, [[0], [0], [0], [0]], [1, 0, 0, 0], [0], tt_s)
+initial_aug = [[theta0], [0], [0], [0]]
+yout, tout, xout = initial(aug_ss, time_o, initial_aug, return_x=True)
+fig, axs = plt.subplots(1, 2)
+axs[0].step(tout, np.rad2deg(xout[:, 0].T), 'r', where='post', label='x1')
+axs[1].step(tout, np.rad2deg(xout[:, 1].T), 'r', where='post', label='x2')
+axs[0].step(tout, np.rad2deg(xout[:, 2].T)+1, 'b', where='post', label='x1_bar+1')
+axs[1].step(tout, np.rad2deg(xout[:, 3].T)+1, 'b', where='post', label='x2_bar+1')
+axs[0].set(xlabel='time', ylabel='theta [deg]')
+axs[1].set(xlabel='time', ylabel='theta_dot [deg/s]')
+axs[0].set_title('theta')
+axs[1].set_title('theta_dot')
+axs[0].legend(loc='best', shadow=True, fontsize='x-large')
+axs[1].legend(loc='best', shadow=True, fontsize='x-large')
+plt.show()
+
+#compensator
+d_A = newA-np.matmul(ss_d.B, K)
+d_B = L
+d_C = K
+d_D = [0]
+G_d = ss2tf(d_A, d_B, d_C, d_D, tt_s)
+print(G_d)
+
+#####
+ref = np.deg2rad(2)
+all_up = np.concatenate((ss_d.A.A, ss_d.B.A), axis=1)
+all_dw = np.concatenate((ss_d.C.A, ss_d.D.A), axis=1)
+all_2 = np.concatenate((all_up, all_dw), axis=0)
+Nrf = np.matmul(np.linalg.inv(all_2), [[0], [0], [1]])
+print(Nrf)
+
+Nx = Nrf[0:2]
+Nu = Nrf[2]
+Nbar = Nu - np.matmul(K, Nx)
+auxB = np.matmul(ss_d.B, Nbar)
+newB = np.concatenate((auxB, auxB), axis=0)
+aug_ss2 = ss(all_A, newB, [1, 0, 0, 0], [0], tt_s)
+print(aug_ss2)
